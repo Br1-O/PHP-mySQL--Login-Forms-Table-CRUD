cad5cc773f8b79f937abbd9363ac073359631171
Br1-O <bruno.ortuno2@gmail.com>
2023-06-03 05:29:29 -0300

    created view for individual company to show comments apart from general view

--- View/show_companies.php
+++ View/show_companies.php
@@ -122,14 +122,12 @@ echo "<table class='table-companies'>";
 while($fila=$fil->fetch_assoc()){
     //imprimir datos en cada fila
   
-    echo"<Tr><Th rowspan ='9' bgcolor='#D3e6f7' id='th-1'>".$fila["nombre"]."</Th>"."<Tr><th>Servicios</Th><Td id='td-1'>".$fila["servicios"]."</Td></Tr>";
+    echo"<Tr><Th rowspan ='8'id='th-1'><a href='show_company.php?id=".$fila['id']."'>".$fila["nombre"]."</a></Th>"."<Tr><th>Servicios</Th><Td id='td-1'>".$fila["servicios"]."</Td></Tr>";
     echo"<Th>Responsable</Th><Td>".$fila["responsable"]."</Td><Tr><Th>Telefono</Th><Td>".$fila["telefono"]."</Td></Tr>";
     echo"<Tr><Th>Pagina</Th><Td>".$fila["pagina"]."</Td></Tr>";
-    echo"<Tr><Th>Comentarios</Th><Td>".$fila["comentarios"]."</Td></Tr>";
+    // echo"<Tr><Th>Comentarios</Th><Td>".$fila["comentarios"]."</Td></Tr>";
     echo"<Tr><Th>Fecha de Inicio</Th><Td>".$fila["fecha_inicio"]."</Td></Tr><Tr><Th>Fecha de Cierre</Th><Td>".$fila["fecha_cierre"]."</Td></Tr>";
     echo"<Tr id='tr-last' ><Th id='th-last' colspan ='2' ><a href='edit_company.php?assoc=".$fila['id']."' style='margin-right:40px;' ><img src='../images/icon_edit.png' alt='edit register' style='width:30px; height:30px margin:5px;' id='btn_edit'></a><a href='../Controller/delete_company.php?idborrar=".$fila['id']."'><img id='btn_delete' src='../images/icon_delete2.png' alt='delete register' style='width:30px; height:30px margin:5px;'></a></Th></Tr>";
-
-
 }
 
 echo "</table>";
